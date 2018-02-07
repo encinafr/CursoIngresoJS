f7362b931d1cd3af36891359b9b9b41bd9bed4e9
encinafr <36193725+encinafr@users.noreply.github.com>
2018-02-07 12:14:03 -0300

    introduccion al if
    primer ejercicio

---
+++ jsInstruccionIF (1).js
@@ -0,0 +1,21 @@
+function Mostrar()
+{
+//tomo la edad  
+   var edad;
+
+
+    edad=document.getElementById('edad').value;
+    
+    edad= parseInt(edad);
+
+
+    if (edad==15)
+    {
+          alert("niña bonita");
+
+    }
+
+
+
+
+}//FIN DE LA FUNCIÓN
