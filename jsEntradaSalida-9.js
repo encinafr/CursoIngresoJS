ad2745bf6880b0eeb7bae422a4391860461d3f26
encinafr <36193725+encinafr@users.noreply.github.com>
2018-02-10 12:25:00 -0300

    ejercicio 9

--- jsEntradaSalida-9.js
+++ jsEntradaSalida-9.js
@@ -4,21 +4,29 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
-   var sueldo;
-   var resultado;
-   var importe10;
+	var sueldo;
+	
 
-   sueldo=document.getElementById('sueldo').value;
-   sueldo=parseInt(sueldo);
+	var resultado;
 
-   resultado=parseInt(resultado);
+	sueldo= parseInt(document.getElementById('sueldo').value);
+
+	resultado = sueldo*1.1;
+
+	document.getElementById('resultado').value= resultado;
+
+	// aL multiplicar por 1.1 es el sueldo + 10 por ciento y si quisiera descontar tendria que restarle 0.1
 
 
-   importe10= sueldo*10/100;
-   resultado=sueldo+importe10;
 
-   document.getElementById('resultado').value=resultado;
-  
+
+	
+
+
+
+
+	
+
 
 	
 }
