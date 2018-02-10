98f6a556c997762f8a8da80ca73b1734defcc17a
encinafr <36193725+encinafr@users.noreply.github.com>
2018-02-10 12:47:59 -0300

    ejercicio 3 del if

---
+++ jsInstruccionIF (3).js
@@ -0,0 +1,30 @@
+function Mostrar()
+{
+//tomo la edad  
+       var edad;
+
+
+    edad=document.getElementById('edad').value;
+    
+    edad= parseInt(edad);
+
+
+    if (edad > 17)
+    {
+          alert("mayor de edad");
+
+          
+     }
+         else
+
+          { 
+
+          	alert(" es menor de edad");
+
+          }
+
+
+
+
+
+}//FIN DE LA FUNCIÓN
