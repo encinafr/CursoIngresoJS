58b16e20365936f20609e4137141bdf3a9e770e9
encinafr <36193725+encinafr@users.noreply.github.com>
2018-02-10 11:45:35 -0300

    ejercicio 7

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,64 @@ mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var numero1;
+	var numero2;
+	var suma;
+
+	numero1= parseInt(document.getElementById('numeroUno').value);
+	numero2= parseInt(document.getElementById('numeroDos').value);
+
+	suma= numero1 + numero2;
+	 alert(" el resultado es: " + suma);
+
+
+
+
+
 }
 
 function restar()
 {
-	
+	var numero1;
+	var numero2;
+	var resta;
+
+	numero1= parseInt(document.getElementById('numeroUno').value);
+	numero2= parseInt(document.getElementById('numeroDos').value);
+
+	resta= numero1 - numero2;
+	 alert(" el resultado es: " + resta);
+
 }
 
 function multiplicar()
 { 
+
+	
+	var numero1;
+	var numero2;
+	var multiplicar;
+
+	numero1= parseInt(document.getElementById('numeroUno').value);
+	numero2= parseInt(document.getElementById('numeroDos').value);
+
+	multiplicar= numero1 * numero2;
+	 alert(" el resultado es: " + multiplicar);
 	
 }
 
 function dividir()
 {
+	var numero1;
+	var numero2;
+	var dividir;
+
+	numero1= parseInt(document.getElementById('numeroUno').value);
+	numero2= parseInt(document.getElementById('numeroDos').value);
+
+	dividir= numero1 / numero2;
+	 alert(" el resultado es: " + dividir);
+	
 	
 }
 
