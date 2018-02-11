1b54c5d20bb4dc43cb5ebab3d8a763aa1853ec0a
encinafr <36193725+encinafr@users.noreply.github.com>
2018-02-11 11:34:10 -0300

    Ejercicio 5 del if

---
+++ jsInstruccionIF (5).js
@@ -0,0 +1,24 @@
+function Mostrar()
+{
+//tomo la edad  
+   var edad;
+
+     edad=document.getElementById('edad').value;
+
+     if(edad<13)
+     {
+     	
+     	alert("No es un adolescente");
+     							
+     }
+     if(edad>17)
+     {
+     	
+     	alert("No es adolescente")
+     }
+      
+     //pregunta si debe mostrar algo cuando ingreses la edad de un adolescente
+
+
+
+}//FIN DE LA FUNCIÓN
