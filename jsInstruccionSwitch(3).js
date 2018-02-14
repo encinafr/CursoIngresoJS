4cb1e4ee825c117986d1bd260f723d011888c8ec
encinafr <36193725+encinafr@users.noreply.github.com>
2018-02-14 12:25:31 -0300

    ejercicio 3 del switch

---
+++ jsInstruccionSwitch(3).js
@@ -0,0 +1,48 @@
+function Mostrar()
+{
+//tomo la edad  
+var mesDelAño = document.getElementById('mes').value;
+
+switch(mesDelAño)
+{
+	case "Febrero":
+
+	       alert("Este mes no tiene mas de 29 dias");
+
+	  break;
+
+	case "Enero":
+	   
+	case "Marzo":
+	   
+	case "Abril":
+
+	case "Mayo":
+
+	case "Junio":
+
+	case "Julio":
+
+	case "Agosto":
+
+	case "Septiembre":
+
+	case "Octubre":
+
+	case "Noviembre":
+
+	case "Diciembre":
+
+	      alert("Este nes tiene 30 dias o mas");
+	       default:
+
+	     // break;
+
+}
+
+
+	
+	
+
+
+}//FIN DE LA FUNCIÓN
