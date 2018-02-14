ade9637ae4c50e6435e3e397746d5227e3cc0f71
encinafr <36193725+encinafr@users.noreply.github.com>
2018-02-14 12:24:55 -0300

    ejercicio 2 del switch

---
+++ jsInstruccionSwitch(2).js
@@ -0,0 +1,32 @@
+function Mostrar()
+{
+//tomo la edad  
+var mesDelAño
+ mesDelAño= document.getElementById('mes').value;
+
+switch(mesDelAño)
+{
+	case "Enero""Febrero":
+
+	       alert("que comiences bien el año");
+
+	  break;
+	case "Marzo":
+	       alert("a clases!!!");
+	   break;
+	case "Julio":
+	       alert("se vienen las vacaciones");
+	       break;
+	case "Diciembre":
+	        alert("Felices Fiestas");             
+}
+
+
+
+alert (mesDelAño);
+
+
+
+
+
+}//FIN DE LA FUNCIÓN
