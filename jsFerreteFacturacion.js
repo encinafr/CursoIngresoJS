a7245e13d3b6ef3eb6b8dcfdc0baa43281e845aa
encinafr <36193725+encinafr@users.noreply.github.com>
2018-02-18 18:26:05 -0300

    ejercicio 1 del tp

---
+++ jsFerreteFacturacion.js
@@ -0,0 +1,54 @@
+/*1.	Para el departamento de facturación:
+A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
+B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
+C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
+*/
+function Sumar () 
+{
+	var precio1;
+	var precio2;
+	var precio3;
+	var suma;
+	precio1=parseInt(document.getElementById('PrecioUno').value);
+	precio2=parseInt(document.getElementById('PrecioDos').value);
+	precio3=parseInt(document.getElementById('PrecioTres').value);
+	
+	suma= precio1 + precio2 + precio3;
+
+	alert(suma);
+
+}
+function Promedio () 
+{
+	var precio1;
+	var precio2;
+	var precio3;
+	var suma;
+	var promedio;
+	precio1=parseInt(document.getElementById('PrecioUno').value);
+	precio2=parseInt(document.getElementById('PrecioDos').value);
+	precio3=parseInt(document.getElementById('PrecioTres').value);
+	
+	suma=  precio1 + precio2 + precio3;
+	promedio= suma/3;
+	alert(promedio);
+}
+function PrecioFinal () 
+{
+	var precio1;
+	var precio2;
+	var precio3;
+	var suma;
+	var iva;
+	var preciofinal;
+	precio1=parseInt(document.getElementById('PrecioUno').value);
+	precio2=parseInt(document.getElementById('PrecioDos').value);
+	precio3=parseInt(document.getElementById('PrecioTres').value);
+	
+	suma= precio1 + precio2 + precio3;
+	iva= suma*0.21;
+	preciofinal= suma + iva;
+
+	alert(preciofinal);
+
+}
