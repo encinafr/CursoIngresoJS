2a3c91bda7236223a315f25eaa9cd44ca32f58d2
encinafr <36193725+encinafr@users.noreply.github.com>
2018-02-20 09:40:00 -0300

    ejercicio 1 y2 del while

---
+++ jsIteraciones (1).js
@@ -0,0 +1,16 @@
+function Mostrar()
+{
+	var contador;
+	var contador1;
+	contador=1;
+
+	while( contador <20)
+	{
+		
+		document.write(contador);
+		contador= contador+1;
+
+
+	}
+
+}//FIN DE LA FUNCIÓN
---
+++ jsIteraciones (2).js
@@ -0,0 +1,17 @@
+function Mostrar()
+{
+	var contador;
+	
+	contador=10;
+
+	while( contador >0)
+	{
+		
+		contador--;
+		document.write("<br>" +contador);
+		
+
+
+	}
+
+}//FIN DE LA FUNCIÓN
