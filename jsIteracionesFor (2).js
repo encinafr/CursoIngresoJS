db0df3aed801d9a5280e0aba909bb658d857ab86
encinafr <36193725+encinafr@users.noreply.github.com>
2018-02-21 12:13:38 -0300

    iteracion3

---
+++ jsIteracionesFor (2).js
@@ -0,0 +1,21 @@
+function Mostrar()
+{
+
+	for( contador=0 ;  ;contador++)
+	{
+		document.write("<br>" + contador);
+		if(contador==10)
+		{
+			break;
+		}
+		if(contador==5)
+		{
+			continue;
+		}
+		document.write("Despues del continue");
+	}
+
+		document.write("fin del for");
+
+
+}
