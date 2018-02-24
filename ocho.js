356677b698c2bacf119d461837fe21e1c2334a59
encinafr <36193725+encinafr@users.noreply.github.com>
2018-02-24 12:10:36 -0300

    ejercicio 8 del parcial

--- ocho.js
+++ ocho.js
@@ -13,6 +13,10 @@ function Mostrar()
     var sexo;
     var sexof=0;
     var sexom=0;
+    var varonesdesaprobados=0;
+    var nombremejornota=1;
+    var edad;
+
 
 
 
@@ -23,6 +27,13 @@ function Mostrar()
 	{
 		nombre = prompt("ingrese el nombre:");
 		cantidad ++;
+		edad = prompt("ingrese su edad");
+		edad = parseInt(edad);
+		if(edad<0 && edad>100)
+		{
+			edad = prompt("ingrese su edad");
+		    edad = parseInt(edad);
+		}
 		sexo = prompt("ingrese su sexo");
 		while(sexo !="f" && sexo !="m")
 		{
@@ -34,10 +45,11 @@ function Mostrar()
 			{
 				sexof ++;
 			}
-			if(sexo=="m")
+			else
 			{
 				sexom ++;
 			}
+
 		
 		nota=prompt("ingrese nota");
 		nota=parseInt(nota);
@@ -48,13 +60,29 @@ function Mostrar()
 			nota=parseInt(nota);
 
 		}
+		if(nota <4 && sexo =="m")
+		{
+           varonesdesaprobados++;
+
+		}
+		if(nota >1)
+		{
+              nombremejornota=nota;
+              nota=nombre;
+              if(!=(nombremejornota<nota))
+              {
+              	nota=nombre;
+              }
+		}
 		seguir = prompt("NO para salir");
 			
 	}
 	promedio= sumadenotas/cantidad;
-	document.write("son" + cantidad + " de alumno");
-	document.write("el promedioes " + promedio + " de notas");
-	document.write("sexom:" + sexom +" sexof:" + sexof);
+	document.write("<br>"+"son" + cantidad + " de alumno");
+	document.write("<br>"+"el promedio es: " + promedio + " de notas");
+	document.write("<br>"+"sexom:" + sexom +" sexof:" + sexof);
+	document.write("<br>"+"los varones desaprobados son:" + varonesdesaprobados);
+	document.write("<br>"+"el nombre de la mejor nota es:" + nota);
 
 	
 
