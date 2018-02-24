0d1483150904743a0cd38e56e44c66e058e4f2b9
encinafr <36193725+encinafr@users.noreply.github.com>
2018-02-24 14:40:16 -0300

    Add files via upload

---
+++ jsIteraciones (3).js
@@ -0,0 +1,19 @@
+function Mostrar()
+{
+
+	var clave ;
+	clave = prompt("ingrese la clave.");
+	var intentos=0;
+	while(clave!="utn750" && intento <2)
+	{
+		var clave = prompt("reingrese la clave.");
+		imtentos++;
+
+	}
+	if(intentos!=2)
+	{
+		alert("la clave es correcta");
+	}
+
+	    
+	}//FIN DE LA FUNCIÓN
---
+++ jsIteraciones (5).js
@@ -0,0 +1,15 @@
+function Mostrar()
+{
+
+var sexo = prompt("ingrese f ó m .");
+
+while(sexo!="f" && sexo !="m")
+{
+	 prompt("ingrese f ó m .");
+}
+
+
+
+document.getElementById('el sexo es:' = sexo).value;
+
+}//FIN DE LA FUNCIÓN
