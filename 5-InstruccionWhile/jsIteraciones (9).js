6e59006024dca26b3c287b07ab330378392e25d7
encinafr <36193725+encinafr@users.noreply.github.com>
2018-02-25 22:43:18 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,16 +1,48 @@
 function Mostrar()
 {
+    var contador=0;
+	var maximo=0;
+	var minimo=0;
 
-	var contador=0;
+	var numero;
 	// declarar variables
 	
 	var respuesta='si';
 
 	while(respuesta!='no')
 	{
+		contador++;
+		numero=prompt("ingrese un numero");
+		numero=parseInt(numero);
 		
-	
+		if(contador==1)
+			{
+				maximo=numero;
+				minimo=numero;
+			}
+		else
+		{	
+
+			if(numero<minimo)
+			{
+				mainimo=numero;
+            }
+			 if(numero>maximo)
+			{
+				maximo=numero;
+			}
+
+
+		}
+
+		respuesta=prompt("para salir, ingresar no");
+		
 	}
+
+	document.getElementById("maximo").value=maximo;
+
+	document.getElementById("minimo").value=minimo;
+
 
 
 
