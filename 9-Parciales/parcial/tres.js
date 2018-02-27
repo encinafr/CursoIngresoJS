64a2ffac6fc9b8d76b9e05693e7764f2d9e07828
encinafr <36193725+encinafr@users.noreply.github.com>
2018-02-27 00:07:58 -0300

    Add files via upload

--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,18 @@
 function Mostrar()
 {
+	var largo;
+	var ancho;
+	var perimetro;
+	var alambre;
+
+	ancho=document.getElementById('alrgo').value;
+    largo=document.getElementById('ancho').value;
+    perimetro= largo*2 + ancho*2;
+
+    alambre= perimetro*3;
+
+    alert("se necesitan:" + alambre + "metros de alambre");
+
+
 
 }
