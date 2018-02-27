4308b7c367d31d29f97dc19ca6d5a2d37f939bab
encinafr <36193725+encinafr@users.noreply.github.com>
2018-02-27 00:54:38 -0300

    Add files via upload

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,33 @@
 function Mostrar()
 {
+	var numero=0;
+	var numero2=0;
+	var suma;
+	var resta;
+	numero=prompt("ingrese numero");
+	numero=parseInt(numero);
+
+	numero2=prompt("ingrese otro numero");
+	numero2=parseInt(numero2);
+	suma= numero + numero2;
+	resta= numero - numero2;
+	if(numero==numero2)
+	{
+	
+	    document.write("el resultado es:" + numero*numero2 );
+	}
+	else
+	{
+		
+	 if(numero>numero2)
+		{
+			 document.write("el resultado es: " + resta );	
+        
+		}
+		 else
+		{
+			document.write("el resultado es: " + suma);	
+		}
+	}
 
 }
