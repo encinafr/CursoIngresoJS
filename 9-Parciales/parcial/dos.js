f1e42513d3bbafb120c1f910da4c06100ec444ea
encinafr <36193725+encinafr@users.noreply.github.com>
2018-02-26 23:58:34 -0300

    Add files via upload

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,15 @@
 function Mostrar()
 {
+	var importe;
+	var importefinal;
+	var iva=0.21;
+
+	importe=prompt("ingrese un importe");
+	importe=parseInt(importe);
+    iva=importe*0.21;
+	importefinal=importe+iva;
+
+	document.getElementById('importeFinal').value=importefinal;
+
   
 }
