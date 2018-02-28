4ebf38bdbae11915ad049654b62f1287a6bfb91d
encinafr <36193725+encinafr@users.noreply.github.com>
2018-02-27 23:58:38 -0300

    Add files via upload

--- 9-Parciales/recuperatorio/cuatro.js
+++ 9-Parciales/recuperatorio/cuatro.js
@@ -1,4 +1,31 @@
 function Mostrar()
 {
+	var primerNumero;
+	var segundoNumero;
+	var sumaNumeros;
+
+	primerNumero = prompt("Ingrese un número:");
+	primerNumero = parseInt(primerNumero);
+
+	segundoNumero = prompt("Ingrese otro número");
+	segundoNumero = parseInt(segundoNumero);
+
+	sumaNumeros = primerNumero + segundoNumero;
+
+	if(sumaNumeros > 0)
+	{
+		document.write("El resultado de la suma es positivo");
+	}
+		else
+		{
+			if(sumaNumeros < 0)
+			{
+				document.write("El resultado de la suma es negativo");
+			}
+			else
+				document.write("El resultado de la suma es 0");
+		}
+
+
 
 }
