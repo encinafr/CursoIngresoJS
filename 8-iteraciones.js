51e8b036f5e336f168d342475230facc0b92725f
encinafr <36193725+encinafr@users.noreply.github.com>
2018-03-03 01:10:21 -0300

    Add files via upload

--- 8-iteraciones.js
+++ 8-iteraciones.js
@@ -1,4 +1,68 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+/*
+function Mostrar()
+{
+	var numero;
+	var contador=0;
+	var respuesta=""
+	var contadorPares=0;
+	var suma=0;
+	var promedio;
+	var minimo;
+	var maximo;
+
+	while(respuesta !="no")
+	{
+		contador++;
+		numero = prompt("Ingrese un número:");
+		numero = parseInt(numero);
+
+		while(numero < 0 || isNaN(numero))
+		{
+			numero = prompt("Ingrese un número:");
+			numero = parseInt(numero);
+		}
+
+		suma = suma + numero;
+
+		if(numero%2==0 && numero!=0)
+		{
+			contadorPares++;
+		}
+
+		if(contador==1)
+		{
+			minimo = numero;
+			maximo = numero;
+		}
+		else
+		{
+			if(numero < minimo)
+			{
+				minimo = numero;
+			}
+			if(numero > maximo)
+			{
+				maximo = numero
+			}
+		}
+
+
+
+		respuesta = prompt("Ingrese no para salir");
+	}	
+
+
+	promedio = suma/contador;
+
+	document.write("La cantidad de números pares es " + contadorPares);
+	document.write("<br> El promedio de los números es " + promedio);
+	document.write("<br> La suma de todos los números ingresados es " + suma);
+	document.write("<br> El número mínimo es " + minimo);
+	document.write("<br> El número máximo es " + maximo);
+}
+*/
+//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 //se ingresa hasta que el usuario quiera, el nombre de un animal, el peso del mismo, validar que sea mayor a cero
 //y la temperatura del habitad de este animal,validar entre menos 40 y 40 informar:la cantidad de temperaturas pares
 //la cantidad de temperaturas impares , el nombre del animal mas pesado, el nombre del animal menos pesado
@@ -12,8 +76,10 @@ function Mostrar()
     var contador=0;
     var temperaturapares=0;
     var temperaturaimpares=0;
-    var nombrepesomaximo=0;
-    var nombrepesominimo=0;
+    var maximo;
+    var minimo;
+    var nombrepesomaximo;
+    var nombrepesominimo;
 	var respuesta="si";
 	while(respuesta == "si")
 	{
@@ -26,6 +92,28 @@ function Mostrar()
 			peso=prompt("ingres el peso del animal");
 		    peso=parseInt(peso);
 		}
+		if(contador==1)
+		{
+	    	 maximo=peso;
+	    	 nombrepesomaximo=nombre;
+		   	 minimo=peso;
+		   	 nombrepesominimo=nombre;
+		}else
+		{
+			if(peso<minimo)//asignale el peso y el nombre
+			{
+				minimo=peso;
+				nombrepesominimo=nombre;
+					
+			}
+
+			if(peso>maximo)
+			{
+				maximo=peso;
+				nombrepesomaximo=nombre;
+					
+			}
+		}	
 		temperatura=prompt("ingrese la temperatura");
 		temperatura=parseInt(temperatura);
 		while(temperatura<-40 || temperatura >40)
@@ -41,37 +129,16 @@ function Mostrar()
 		{
              temperaturaimpares++;
 		}
-		if(contador==1)
-		{
-			peso=nombre;
-			peso=nombre;
-		}else
-		{
-			if(peso<nombrepesominimo)//asignale el peso y el nombre
-			{
-				nombrepesominimo=peso;
-				nombrepesominimo=nombre;	
-			}
-
-			if(peso>nombrepesomaximo)
-			{
-				nombrepesomaximo=peso;
-				nombrepesomaximo=nombre;
-					
-			}
-			
-
-			
-		}
        respuesta=prompt("desea continuar, ingrese si");
 	}
 
 		
-  document.write("<br> el nombre del animal con peso maximo es:" + nombre);
-  document.write("<br> el nombre del animal con peso minimo es:" + nombre);
+  document.write("<br> el nombre del animal con peso maximo es:" + nombrepesomaximo);
+  document.write("<br> el nombre del animal con peso minimo es:" + nombrepesominimo);
   document.write("<br> las temperaturapares son: "  + temperaturapares);
   document.write("<br> las temperaturaimpares son: "  + temperaturaimpares);
 
 
 }
+
 
